9f2004f0e4d5868d9960433068ca545043dc4c87
guahunyo <2736859895@qq.com>
2017-10-14 13:50:33 +0800

    test

---
+++ socket/pages/index/index.js
@@ -0,0 +1,39 @@
+var wsApi = "ws://192.168.1.105:3001/username";
+var openBol = false;
+Page({
+  data: {
+    
+  },
+  onLoad:function(){
+    wx.connectSocket({
+      url: wsApi,
+      data:{
+        username:"111"
+      },
+      header:{ 
+        'content-type': 'application/json'
+      },
+      protocols: ['protocol1'],
+      method:"GET",
+      success:function(){
+        console.log("客户端连接成功");
+      }
+    }),
+    wx.onSocketOpen(function(res) {
+      console.log('WebSocket连接已打开！');
+      openBol = true;
+    }),
+    wx.onSocketMessage(function(res) {
+      console.log('收到服务器内容：' + res.data)
+    })
+
+  },
+  sendMsg:function(e){
+    if(openBol){
+      wx.sendSocketMessage({
+        data:e.detail.value
+      });
+    }
+  }
+  
+})
